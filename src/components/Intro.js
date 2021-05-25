17d725a01d93579bcc2f079595a348ca0e9cf531
Fredrik <fredrikf2000@gmail.com>
2021-05-25 15:25:32 +0200

    started working intro section

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,5 +1,24 @@
 const Intro = () => {
-  return <div></div>;
+  return (
+    <header>
+      <div className="logo">
+        <h3 className="logo__item">Fredrik Fordelsen</h3>
+      </div>
+
+      <button className="toggleNav">
+        <span className="menuBtn"></span>
+      </button>
+
+      <nav className="nav">
+        <ul className="nav__list">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Intro;
