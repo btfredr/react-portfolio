aaaa559f62217b1ed88924558f06f19e01e46150
Fredrik <fredrikf2000@gmail.com>
2021-05-31 01:00:54 +0200

    trying to make nav open onClick

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,41 @@
-import React from "react";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [openNav, setOpenNav] = useState(false);
+
   return (
-    <nav className="nav">
-      <div className="nav__left">
-        <div className="nav__links">
-          <a href="#home">Home</a>
-          <a href="#experience">Experience</a>
-          <a href="#projects">Projects</a>
-          <a href="#aboutMe">About</a>
-        </div>
+    <header>
+      <div class="logo">
+        <h3 class="logo__item">Fredrik Fordelsen</h3>
       </div>
-      <div className="nav__right">Fredrik Fordelsen</div>
-    </nav>
+      <button class="toggleNav" onClick={() => setOpenNav(!openNav)}>
+        <span class="hamburger"></span>
+      </button>
+      <nav class="nav">
+        <ul class={openNav ? "nav-open" : ""}>
+          <li class="nav__item">
+            <a href="#home" class="nav__link">
+              Home
+            </a>
+          </li>
+          <li class="nav__item">
+            <a href="#experience" class="nav__link">
+              Experience
+            </a>
+          </li>
+          <li class="nav__item">
+            <a href="#projects" class="nav__link">
+              Projects
+            </a>
+          </li>
+          <li class="nav__item">
+            <a href="#aboutMe" class="nav__link">
+              About me
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
